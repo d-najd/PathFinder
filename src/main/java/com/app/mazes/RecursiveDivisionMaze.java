1edc5a256b3535064852c651e55bd91041c1f38b
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-02-20 18:31:36 +0100

    feat: added animation to RecursiveDivisionMaze

--- src/main/java/com/app/mazes/RecursiveDivisionMaze.java
+++ src/main/java/com/app/mazes/RecursiveDivisionMaze.java
@@ -3,6 +3,7 @@ package com.app.mazes;
 import java.util.List;
 import java.util.Random;
 
+import com.app.Settings;
 import com.app.data.Piece;
 import com.app.ui.DrawGrid;
 
@@ -27,44 +28,48 @@ public class RecursiveDivisionMaze implements IMaze {
 		var remainingHeight = grid.getFirst().size();
 
 		var splitType = determineSplitType(remainingWidth, remainingHeight);
+		var remainingSize = splitType == SplitType.Horizontal ? remainingHeight : remainingWidth;
+		var remainingSizeOpposite = splitType == SplitType.Vertical ? remainingHeight : remainingWidth;
+
+		if (remainingSize <= 2) {
+			return;
+		}
+
+		var splitIndex = new Random().nextInt(remainingSize - 2);
+		var pathPieceIndex = new Random().nextInt(remainingSizeOpposite);
+
+		for (int i = 0; i < remainingSizeOpposite; i++) {
+			if (i == pathPieceIndex) {
+				continue;
+			}
+
+			var curPiece = splitType == SplitType.Horizontal ? grid.get(i).get(splitIndex + 1)
+					: grid.get(splitIndex + 1).get(i);
+
+			if (!MazeUtils.isEmptyPiece(curPiece)) {
+				continue;
+			}
+
+			curPiece.setType(Piece.Type.Wall);
+			gridObj.addPiecesForRepainting(curPiece);
+
+			try {
+				// noinspection BusyWait
+				Thread.sleep(Settings.MAZE_GEN_SPEED);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
 
 		if (splitType == SplitType.Horizontal) {
-			if (remainingHeight <= 2) {
-				return;
-			}
-
-			var splitIndex = new Random().nextInt(remainingHeight - 2);
-			var pathPieceIndex = new Random().nextInt(remainingWidth);
-
-			for (int i = 0; i < remainingWidth; i++) {
-				if (i == pathPieceIndex) {
-					continue;
-				}
-
-				MazeUtils.setPieceToWallIfEmpty(grid.get(i).get(splitIndex + 1));
-			}
-
 			divide(grid.stream().map(o -> o.subList(0, splitIndex)).toList(), gridObj);
-			divide(grid.stream().map(o -> o.subList(splitIndex + 2, Math.max(splitIndex + 2, remainingHeight))).toList(),
+			divide(grid.stream().map(o -> o.subList(splitIndex + 2, Math.max(splitIndex +
+					2, remainingHeight))).toList(),
 					gridObj);
 		} else {
-			if (remainingWidth <= 2) {
-				return;
-			}
-
-			var splitIndex = new Random().nextInt(remainingWidth - 2);
-			var pathPieceIndex = new Random().nextInt(remainingHeight);
-
-			for (int i = 0; i < remainingHeight; i++) {
-				if (i == pathPieceIndex) {
-					continue;
-				}
-
-				MazeUtils.setPieceToWallIfEmpty(grid.get(splitIndex + 1).get(i));
-			}
-
 			divide(grid.subList(0, splitIndex), gridObj);
-			divide(grid.subList(splitIndex + 2, Math.max(splitIndex + 2, remainingWidth)), gridObj);
+			divide(grid.subList(splitIndex + 2, Math.max(splitIndex + 2,
+					remainingWidth)), gridObj);
 		}
 	}
 
